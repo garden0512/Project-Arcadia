9dfa0402e3d2fbe8d3351e5ee182957b38db462a
Garden1205 <12gundam05@gmail.com>
2025-07-24 18:08:14 +0900

    인벤토리 기능들

--- Arcadia/Assets/Scripts/UI/Inventory3/InventoryUI.cs
+++ Arcadia/Assets/Scripts/UI/Inventory3/InventoryUI.cs
@@ -5,7 +5,7 @@ namespace Arcadia.UI.Inventory3
     public class InventoryUI : MonoBehaviour
     {
         [SerializeField] private InventorySlotUI _inventorySlotUI;
-        private Transform _slotPanel;
+        [SerializeField] private Transform _slotPanel;
         private InventoryViewModel _inventoryViewModel;
         private InventorySlotUI[] _slotUIs;
         
@@ -14,21 +14,48 @@ namespace Arcadia.UI.Inventory3
         /// </summary>
         public void Bind(InventoryViewModel inventoryViewModel)
         {
-            
+            _inventoryViewModel = inventoryViewModel;
+            int SlotCount = _inventoryViewModel.InventorySlotViewModels.Count;
+            _slotUIs = new InventorySlotUI[SlotCount];
+            for (int i = 0; i < SlotCount; i++)
+            {
+                var slotViewModel = _inventoryViewModel.InventorySlotViewModels[i];
+                GameObject slotObject = new GameObject($"InventorySlotUI_{i}");
+                slotObject.transform.SetParent(_slotPanel, false);
+                var slotUI = slotObject.AddComponent<InventorySlotUI>();
+                slotUI.Bind(slotViewModel);
+                _slotUIs[i] = slotUI;
+            }
         }
         /// <summary>
         /// 이벤트 구독 해제
         /// </summary>
         public void OnDestroySubscribe()
         {
-            //당장은 쓸모없을듯?
+            if (_slotUIs == null)
+            {
+                return;
+            }
+
+            foreach (var slotUI in _slotUIs)
+            {
+                slotUI.OnDestroySubscribe();
+            }
         }
         /// <summary>
         /// 전체 UI를 최신상태로 갱신
         /// </summary>
         public void RefreshAll()
         {
-            
+            if (_slotUIs == null)
+            {
+                return;
+            }
+
+            foreach (var slotUI in _slotUIs)
+            {
+                slotUI.UpdateUI();
+            }
         }
     }
 }
