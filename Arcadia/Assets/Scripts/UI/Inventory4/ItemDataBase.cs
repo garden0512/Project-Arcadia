5ad6062904fa883e2401f12ea8d4590fa9483fd0
Garden1205 <12gundam05@gmail.com>
2025-07-30 19:49:27 +0900

    Revert "인벤토리 생성 구현"
    This reverts commit 60c2daecf99fc6558361df192f17a6d1882ec533.
    
    # Conflicts:
    #	Arcadia/Assets/Scenes/Testing.unity
    #	Arcadia/Assets/Scripts/UI/Inventory4/ItemDataBase.cs

---
+++ Arcadia/Assets/Scripts/UI/Inventory4/ItemDataBase.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using LitJson;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Arcadia.UI.Inventory4
+{
+    public class ItemDataBase : MonoBehaviour
+    {
+        private List<Item> _items = new List<Item>();
+        private JsonData _jsonData;
+
+        private void Start()
+        {
+            _jsonData = JsonMapper.ToObject(File.ReadAllText(Application.dataPath + "/StreamingAssets/Items.json"));
+        }
+    }
+
+    public class Item
+    {
+        
+    }
+}
