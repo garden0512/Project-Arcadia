5be125e29cf7cb1f5eb120120533079255b1052b
Garden0512 <12gundam05@gmail.com>
2025-09-11 13:59:14 +0900

    스테이지 시작 연출 효과 스크립트

---
+++ Arcadia/Assets/Scripts/Prototype/ComprehensiveEffectPrototypeVer1/StageStartBlinkText.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using TMPro;
+using System.Collections;
+
+namespace Arcadia.Prototype.ComprehensiveEffectPrototypeVer1
+{
+    public class StageStartBlinkText : MonoBehaviour
+    {
+        public TextMeshProUGUI stageNumber;
+        public TextMeshProUGUI stageName;
+
+        public void Start()
+        {
+            stageName.gameObject.SetActive(false);
+            stageNumber.gameObject.SetActive(false);
+            StartCoroutine(DelayAndBlinkText());
+        }
+
+        IEnumerator DelayAndBlinkText()
+        {
+            yield return new WaitForSeconds(2f);
+            stageNumber.gameObject.SetActive(true);
+            yield return new WaitForSeconds(1f);
+            stageName.gameObject.SetActive(true);
+            yield return new WaitForSeconds(2f);
+            stageNumber.gameObject.SetActive(false);
+            stageName.gameObject.SetActive(false);
+        }
+    }
+}
